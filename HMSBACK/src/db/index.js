a55a7a11d5053f44f48d6d71cad79d34bcbcbfe9
sananasgarov <prog58816@gmail.com>
2026-03-15 14:57:46 +0400

    feat: Add initial MongoDB connection setup using Mongoose.

--- HMSBACK/src/db/index.js
+++ HMSBACK/src/db/index.js
@@ -1,27 +1,14 @@
 const mongoose = require("mongoose");
 const config = require("../config");
-
-let isConnected = false;
-
-const db = async () => {
-  if (isConnected) {
-    return;
-  }
-
-  try {
-    const conn = await mongoose.connect(
-      config.mongodb.url.replace("<password>", config.mongodb.pass),
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-
-    isConnected = conn.connections[0].readyState;
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
+const db = () => {
+    mongoose
+        .connect(config.mongodb.url.replace("<password>", config.mongodb.pass))
+        .then(() => {
+            console.log("MongoDB connected");
+        })
+        .catch((err) => {
+            console.error("MongoDB connection error:", err);
+        });
 };
 
 module.exports = db;
